32333e4fc5db2e7a464bba0952316ef2cf3a448d
Ysabel Garcia <ysabel.garcia@live.com>
2021-09-13 23:02:43 +0000

    queries

---
+++ 1_queries/2_total_students_cohort.sql
@@ -0,0 +1,3 @@
+SELECT count(*)
+FROM students
+WHERE cohort_id >= 1 AND cohort_id <= 3;
---
+++ 1_queries/4_non_gmail.sql
@@ -0,0 +1,3 @@
+SELECT name, email, id, cohort_id
+FROM students
+WHERE email NOT LIKE '%gmail%' AND phone IS NULL;
