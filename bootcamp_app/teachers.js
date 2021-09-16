dfc5ab5fd7f5d9ae82629248a2ea6e63d8507124
Ysabel Garcia <ysabel.garcia@live.com>
2021-09-16 05:41:07 +0000

    teachers and students.js made parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,35 @@ const pool = new Pool({
 });
 
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers 
-JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name = 'JUL02'
-ORDER BY teachers.name;
-`)
+// pool.query(`
+// SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+// FROM teachers 
+// JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
+// JOIN students ON students.id = assistance_requests.student_id
+// JOIN cohorts ON cohorts.id = students.cohort_id
+// WHERE cohorts.name = 'JUL02'
+// ORDER BY teachers.name;
+// `)
+// .then(res => {
+//   res.rows.forEach(row => {
+//     console.log(`${row.cohort}: ${row.teacher}`);
+//   });
+// });
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
